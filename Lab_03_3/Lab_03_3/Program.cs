2fd4df818167be0586600a239f88144aba3c12c8
sqrad <voznyak.ed@gmail.com>
2024-10-06 16:28:39 +0300

    Add files via upload

---
+++ Lab_03_3/Lab_03_3/Program.cs
@@ -0,0 +1,32 @@
+﻿// Lab_03_3.cs
+// <Возняк Едвард>
+// Лабораторна робота №3.3
+// Розгалуження, задане графіком функції.
+// Варіант 5
+class Lab3_3
+{
+    static void Main(string[] args)
+    {
+        double y, x;
+        double R;
+
+        Console.WriteLine("Enter x value:");
+        x = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter R value:");
+        R = double.Parse(Console.ReadLine());
+
+        if (-4 < x && x < -2)
+            y = x + 3;
+        else if (-2 < x && x < 4)
+            y = 1 + ((-R - 1) / (4 + 2)) * (x + 2);
+        else if (4 < x && x < 8 - R)
+            y = -R;
+        else if(8-R<x && x < 8+R)
+            y = -R + Math.Sqrt(Math.Pow(R,2) - Math.Pow(x - 8,2));
+        else
+            y = -R;
+
+        Console.WriteLine($"y = {y:F8}");
+    }
+}
---
+++ Lab_03_4/Lab_03_4/Program.cs
@@ -0,0 +1,29 @@
+﻿// Lab_03_4.cs
+// <Возняк Едвард>
+// Лабораторна робота №3.4
+// Розгалуження, задане плоскою фігурою.
+// Варіант 5
+class Lab3_4
+{
+    static void Main(string[] args)
+    {
+        double x;
+        double y;
+        double R;
+
+        Console.WriteLine("Enter x value:");
+        x = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter y value:");
+        y = double.Parse(Console.ReadLine());
+        Console.WriteLine("Enter R value:");
+        R = double.Parse(Console.ReadLine());
+
+        if (x >= 0 && y >= 0 && Math.Pow(x + y, 2) <= Math.Pow(R, 2) ||
+            x <= 0 && y <= 0 && Math.Pow(x + y, 2) <= Math.Pow(R, 2) ||
+            x <= 0 && y >= 0 && Math.Pow(x + y, 2) <= Math.Pow(R, 2) && y >= -x)
+            Console.WriteLine("Yes");
+        else
+            Console.WriteLine("No");
+
+    }
+}
